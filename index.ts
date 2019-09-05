c8b96e429768060bdf4ac7f5f07c654d00ebf1d0
Michael Ord <michael.ord@gmail.com>
2019-09-05 13:02:47 +0100

    code updates

---
+++ index.ts
@@ -0,0 +1,7 @@
+export type Feedback = 'error' | 'success' | 'default' | 'warning' | 'info';
+export type Padding = 'default' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
+export type Size = 'default' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl' | 'full';
+export type Priority = 'default' | 'primary' | 'secondary' | 'tertiary';
+export type Align = 'left' | 'center' | 'right';
+export type HeadingSize = 1 | 2 | 3 | 4 | 5 | 6;
+export type Theme = 'default' | 'primary' | 'dark' | 'light';
